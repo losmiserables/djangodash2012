456bf650d0c6c487616869986ca29bab4768b0cb
Dalton Barreto <daltonmatos@gmail.com>
2012-08-18 10:33:24 -0300

    Choose which branch will be deployed

--- dash2012/fabfile.py
+++ dash2012/fabfile.py
@@ -5,10 +5,11 @@ env.use_ssh_config = True
 env.ssh_config_path = '~/.ssh/config'
 
 
-def deploy():
+def deploy(branch='master'):
     with settings(host_string='dash.daltonmatos.com'):
         with cd("/var/mongrel2/apps/djangodash2012.daltonmatos.com"):
             with cd("app/djangodash2012/"):
                 run("git reset --hard")
-                run("git pull origin master")
+                run("git checkout {branch}".format(branch=branch))
+                run("git pull origin {branch}".format(branch=branch))
             run("wsgid restart")
