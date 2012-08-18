0fd75ce00eb9acad60c62591b6e2016c1ad12cae
Dalton Barreto <daltonmatos@gmail.com>
2012-08-18 15:30:10 -0300

    Basic login/logout logic

--- dash2012/cloudfish/views.py
+++ dash2012/cloudfish/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
+    if request.user.is_authenticated():
+        return render(request, 'index.html', {'greetings':'Hi!'})
     return render(request, 'index.html')
 
+
+@login_required(login_url='/auth/login')
 def account(request):
-    return render(request, 'account.html')
+    return render(request, 'account.html', {'active_account': 'active'})
