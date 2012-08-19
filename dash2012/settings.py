0f4c99741b321397f9aa1ffbdf084dbaf26cb58a
Dalton Barreto <daltonmatos@gmail.com>
2012-08-19 00:36:40 -0300

    Added analytics code

--- dash2012/settings.py
+++ dash2012/settings.py
@@ -38,6 +38,8 @@ CACHES = {
     }
 }
 
+ANALYTICS = os.environ.get('ANALYTICS', '')
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
