548af7d6dcc0bda976bb0b40bbbc0b618dca996d
Dalton Barreto <daltonmatos@gmail.com>
2012-08-18 23:43:43 -0300

    Added session engine and cache configs

--- dash2012/settings.py
+++ dash2012/settings.py
@@ -29,6 +29,14 @@ DATABASES = {
 }
 
 LOGIN_URL = '/auth/login'
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
